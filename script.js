34b884540b53666a8ad5d0b4a4b4eaa828ab8102
Eduardo Amaral <eduardo.lopes.amaralxx@gmail.com>
2021-10-23 16:48:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function applyBlack(){
+    document.getElementById("trocarimg").src="/colors/black.png";
+}
+
+function applyBlue(){
+    document.getElementById("trocarimg").src="/colors/blue.png";
+}
+
+function applyRed(){
+    document.getElementById("trocarimg").src="/colors/red.png";
+}
+function applyYellow(){
+    document.getElementById("trocarimg").src="/colors/yellow.png";
+}
+function applyOrange(){
+    document.getElementById("trocarimg").src="/colors/orange.png";
+}
+function applyPink(){
+    document.getElementById("trocarimg").src="/colors/pink.png";
+}
+function applyGreen(){
+    document.getElementById("trocarimg").src="/colors/green.png";
+}
